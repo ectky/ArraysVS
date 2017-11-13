c30c82368e3ca8fb285987e9fc1a3adda67fb593
Elitsa <ectky3@gmail.com>
2017-11-13 17:39:04 +0200

    Add files via upload

---
+++ IntegerToBase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerToBase
+{
+    class Program
+    {
+        static string IntegerToBase(int number, int toBase)
+        {
+            int remeinder = 0;
+            string converted = "";
+            while (number != 0)
+            {
+                remeinder = number % toBase;
+                converted = (char)(remeinder + 48) + converted;
+                number /= toBase;
+            }
+            return converted;
+        }
+
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int toBase = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(IntegerToBase(number, toBase));
+        }
+    }
+}
---
+++ Last3ConsecutiveEqualStrings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last3ConsecutiveEqualStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ').ToArray();
+            string previous = ""; bool checker = false;
+            string str = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == previous && checker)
+                {
+                    str = array[i];
+                }
+                if (array[i] == previous)
+                {
+                    checker = true;
+                }
+                else
+                {
+                    checker = false;
+                }
+                previous = array[i];
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write(str + ' ');
+            }
+            Console.Write(str);
+
+        }
+    }
+}
